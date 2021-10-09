feeda44cd6e14448e76e52d0b135b454da37cfb6
mzhratul <mdzahid.hr@gmail.com>
2021-10-09 17:38:41 +0600

    Navbar html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 $(function () {
    'use strict';
+
+   /* Top Header New Slider Js Start */
+   $('.top_header_news').slick({
+      arrows: false,
+      vertical: true,
+      verticalSwiping: true,
+      autoplay:true,
+   });
+   /* //Top Header New Slider Js End */
 })
